7d679fcaa70d3e8f8bf45c8947be46de76b07525
Марат Белицкий <marat.belitckiy2010@yandex.com>
2023-12-14 08:58:14 +0300

    TMS_AnOnl_17_Java Lesson_1

---
+++ src/lesson_1/Main.java
@@ -0,0 +1,32 @@
+package lesson_1;
+import java.util.Scanner;
+
+/**
+ * @author Belitski Marat
+ * @date 14.12.2023
+ * @project TMS_AnOnl_17_Java
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Задача №1
+        System.out.println("Hello world!");
+        System.out.println();
+
+        //Задача №2
+        System.out.println("Введите имя пользователя: ");
+        System.out.println("Hello " + scanner.nextLine() + "!");
+        System.out.println();
+
+        //Задача №3
+        System.out.println("Считаем сумму двух чисел.");
+        System.out.println("Введите первое число: ");
+        int numberA = scanner.nextInt();
+
+        System.out.println("Введите второе число: ");
+        int numberB = scanner.nextInt();
+        System.out.println("Сумма чисел " + numberA + " и " + numberB + " равна " + (numberA + numberB));
+    }
+}
