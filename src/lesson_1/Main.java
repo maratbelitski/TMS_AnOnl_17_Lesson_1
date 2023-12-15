1d6bd5fc537c6a24d456cf76929a30b249be5ef1
Марат Белицкий <marat.belitckiy2010@yandex.com>
2023-12-15 08:10:48 +0300

    TMS_AnOnl_17_Java Lesson_1

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -12,21 +12,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        //Задача №1
-//        System.out.println("Hello world!");
-//        System.out.println();
-//
-//        //Задача №2
-//        System.out.println("Введите имя пользователя: ");
-//        System.out.println("Hello " + scanner.nextLine() + "!");
-//        System.out.println();
+        //Задача №1
+        System.out.println("Hello world!");
+        System.out.println();
+
+        //Задача №2
+        System.out.print("Введите имя пользователя: ");
+        System.out.print("Hello " + scanner.nextLine() + "!");
+        System.out.println();
 
         //Задача №3
-        System.out.print("Считаем сумму двух чисел.");
+        System.out.println("Считаем сумму двух чисел.");
         System.out.print("Введите первое число: ");
         int numberA = scanner.nextInt();
 
-        System.out.println("Введите второе число: ");
+        System.out.print("Введите второе число: ");
         int numberB = scanner.nextInt();
         System.out.println("Сумма чисел " + numberA + " и " + numberB + " равна " + (numberA + numberB));
 
